0c9488a92afa5ee158a1ccc4612ad00c7b4ccf1a
KaziMdMehediHasan <=>
2022-01-02 00:14:00 +0600

    style added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {Button,Menu,Typography,Avatar} from 'antd';
 import {Link} from 'react-router-dom';
-import {HomeOutLined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenusOutlined} from '@ant-design/icons';
+import {HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenusOutlined} from '@ant-design/icons';
 import icon from '../images/cryptocurrency.png';
+
 function Navbar() {
     return (
         <div className="nav-container">
@@ -13,6 +14,20 @@ function Navbar() {
                 </Typography.Title>
                 {/* <Button className="menu-control-container"></Button> */}
             </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined/>}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined/>}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined/>}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined/>}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }

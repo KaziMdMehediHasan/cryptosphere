762ffb4e50fd20bb3d860a18e61bfcacde37de7f
monzu25 <monzu.ctg25@gmail.com>
2022-01-02 14:21:59 +0600

    this is Monzu

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
 
-function Cryptocurrencies() {
-    return (
+import { useGetCryptoQuery } from "../services/cryptoApi";
+
+function Cryptocurrencies({ simplified }) {
+  const count = simplified ? 10 : 100;
+
+  const { data: cryptosList, isFetching } = useGetCryptoQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    setCryptos(cryptosList?.data?.coins);
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowercase().includes(searchTerm.toLowerCase())
+    );
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
+
+  console.log(cryptos);
+  if (isFetching) return "Loading...";
+
+  return (
+    <>
+      {!simplified && (
         <div>
-            Cryptocurrencies
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          ></Input>
         </div>
-    )
+      )}
+
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
+            <Link to={`/crypto/${currency.id}`}>
+              <Card
+                title={`${currency.rank}.${currency.name}`}
+                extra={<img className="crypto-image" src={currency.iconUrl} />}
+              >
+                <p> Price: {millify(currency.price)}</p>
+                <p> Market Cap: {millify(currency.marketCap)}</p>
+                <p> Daily Change: {millify(currency.change)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,33 +1,75 @@
-import React from 'react'
-import millify from 'millify';
-import {Typography, Row, Col, Statistic} from 'antd';
-import {Link} from 'react-router-dom';
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic } from "antd";
+import { Link } from "react-router-dom";
+import { useGetCryptoQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
 
-const {Title} = Typography;
+const { Title } = Typography;
 
 function Homepage() {
-    return (
-        <>
-          <Title level={2} className='heading'>Global Crypto Stats</Title>
-          <Row>
-              <Col span={12}>
-                  <Statistic title='Total Cryptocurrencies' value='5' />
-              </Col>
-              <Col span={12}>
-                  <Statistic title='Total Exchanges' value='5' />
-              </Col>
-              <Col span={12}>
-                  <Statistic title='Total Market Cap' value='5' />
-              </Col>
-              <Col span={12}>
-                  <Statistic title='Total 24h Volume' value='5' />
-              </Col>
-              <Col span={12}>
-                  <Statistic title='Total Markets' value='5' />
-              </Col>
-          </Row>
-        </>
-    )
+  const { data, isFetching } = useGetCryptoQuery(10);
+  const globalStatus = data?.data?.status;
+
+  console.log(data);
+  if (isFetching) return "Loading...";
+
+  return (
+    <>
+      <Title level={2} className="heading">
+        Global Crypto Stats
+      </Title>
+      <Row>
+        <Col span={12}>
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={globalStatus.total}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Exchanges"
+            value={globalStatus.totalExchanges}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Market Cap"
+            value={globalStatus.totalMarketCap}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total 24h Volume"
+            value={globalStatus.total24Volume}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic title="Total Markets" value={globalStatus.totalMarkets} />
+        </Col>
+      </Row>
+
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptocurrencies
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
+    </>
+  );
 }
 
-export default Homepage
+export default Homepage;

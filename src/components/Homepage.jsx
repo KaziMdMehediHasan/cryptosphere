35e8c81ed7db86e850d2b44148a864693cdd3f88
monzu25 <monzu.ctg25@gmail.com>
2022-01-03 09:11:36 +0600

    this is monzu

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -2,74 +2,97 @@ import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
-import { useGetCryptoQuery } from "../services/cryptoApi";
-import { Cryptocurrencies, News } from "../components";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import Cryptocurrencies from "./Cryptocurrencies";
+import News from "./News";
+import Loader from "./Loader";
 
 const { Title } = Typography;
 
-function Homepage() {
-  const { data, isFetching } = useGetCryptoQuery(10);
-  const globalStatus = data?.data?.status;
+const Homepage = () => {
+  const { data, isFetching } = useGetCryptosQuery(10);
+  const globalStats = data?.data?.stats;
 
-  console.log(data);
-  if (isFetching) return "Loading...";
+  if (isFetching) return <Loader />;
+
+  const plateStyles = {
+    boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+    textAlign: "center",
+    borderRadius: "50px",
+    color: "#001529",
+    fontWeight: "bold",
+  };
 
   return (
     <>
       <Title level={2} className="heading">
         Global Crypto Stats
       </Title>
-      <Row>
+      <Row gutter={[32, 32]}>
         <Col span={12}>
           <Statistic
+            style={plateStyles}
             title="Total Cryptocurrencies"
-            value={globalStatus.total}
+            value={globalStats.total}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Exchanges"
-            value={globalStatus.totalExchanges}
+            value={millify(globalStats.totalExchanges)}
+            style={plateStyles}
           />
         </Col>
         <Col span={12}>
           <Statistic
-            title="Total Market Cap"
-            value={globalStatus.totalMarketCap}
+            title="Total Market Cap:"
+            value={`$${millify(globalStats.totalMarketCap)}`}
+            style={plateStyles}
           />
         </Col>
         <Col span={12}>
           <Statistic
             title="Total 24h Volume"
-            value={globalStatus.total24Volume}
+            value={`$${millify(globalStats.total24hVolume)}`}
+            style={plateStyles}
           />
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets" value={globalStatus.totalMarkets} />
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={globalStats.total}
+            style={plateStyles}
+          />
+        </Col>
+        <Col span={12}>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats.totalMarkets)}
+            style={plateStyles}
+          />
         </Col>
       </Row>
-
       <div className="home-heading-container">
         <Title level={2} className="home-title">
-          Top 10 Cryptocurrencies
+          Top 10 Cryptos In The World
         </Title>
         <Title level={3} className="show-more">
-          <Link to="/cryptocurrencies">Show More</Link>
+          <Link to="/cryptocurrencies">Show more</Link>
         </Title>
       </div>
-
       <Cryptocurrencies simplified />
       <div className="home-heading-container">
         <Title level={2} className="home-title">
           Latest Crypto News
         </Title>
-        <Title level={3} className="show-more">
-          <Link to="/news">Show More</Link>
+        <Title level={3}>
+          <Link to="/news">Show more</Link>
         </Title>
       </div>
       <News simplified />
     </>
   );
-}
+};
 
 export default Homepage;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,43 +1,41 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// Note: Change v1 to v2 on rapid api
+
 const cryptoApiHeaders = {
-  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-  "x-rapidapi-key": "937bec4bf0msh83f9e3d0dad17fdp11a0bcjsn40209f624f65",
+  "x-rapidapi-host": process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
+  "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
 };
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
-const baseUrl = "https://coinranking1.p.rapidapi.com";
-
-const createRequest = (url) => ({
-  url,
-  headers: cryptoApiHeaders,
-});
-export const cryptoApi = {
+export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+
+    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
+    getCryptoHistory: builder.query({
       query: ({ coinId, timeperiod }) =>
         createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
-      //   query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+
+    // Note: To access this endpoint you need premium plan
+    getExchanges: builder.query({
+      query: () => createRequest("/exchanges"),
     }),
   }),
-};
+});
 
-export const { useGetCryptoQuery } = cryptoApi;
-
-// var options = {
-//   method: "GET",
-//   url: "https://coinranking1.p.rapidapi.com/exchanges",
-//   params: {
-//     referenceCurrencyUuid: "yhjMzLPhuIDl",
-//     uuids: "-zdvbieRdZ",
-//     limit: "50",
-//     offset: "0",
-//     orderBy: "24hVolume",
-//     orderDirection: "desc",
-//   },
-//   headers: {
-//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-//     "x-rapidapi-key": "937bec4bf0msh83f9e3d0dad17fdp11a0bcjsn40209f624f65",
-//   },
-// };
+export const {
+  useGetCryptosQuery,
+  useGetCryptoDetailsQuery,
+  useGetExchangesQuery,
+  useGetCryptoHistoryQuery,
+} = cryptoApi;

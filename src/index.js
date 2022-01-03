3a55e22d3f90b0ea23ac0d8212e3de354489024f
KaziMdMehediHasan <=>
2022-01-03 22:45:39 +0600

    chart and news fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
 
-import App from "./App";
-import store from "./app/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import "antd/dist/antd.css";
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,5 +17,6 @@ ReactDOM.render(
       </Provider>
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
+

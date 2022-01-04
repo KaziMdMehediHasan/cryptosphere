d5073af531b9bbe510b1e6f66157747732d0740f
KaziMdMehediHasan <=>
2022-01-04 22:06:38 +0600

    layout bug fixed

--- src/components/Homepage.test.js
+++ src/components/Homepage.test.js
@@ -1,14 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import Homepage from "./Homepage";
-test('renders Global Crypto Stats as a text', ()=>{
-    //arrange
-    render(<Homepage/>);
 
-    //act
-    //..nothing
+describe("Homepage Component", () => {
+    test('renders Global Crypto Stats as a text', ()=>{
+        //arrange
+        render(<Homepage/>);
+    
+        //act
+        //..nothing
+    
+        // assert
+        const cryptoGlobal = screen.getByText('Crypto Global Stats');
+    
+        expect(cryptoGlobal).toBeInTheDocument();
+    });
 
-    // assert
-    const cryptoGlobal = screen.getByText('Crypto Global Stats');
+    // test('if there is avatar component', ()=>{
+    //     render(<Homepage/>);
 
-    expect(cryptoGlobal).toBeInTheDocument();
-});
+    //     screen.
+    // });
+})

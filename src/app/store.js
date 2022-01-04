ceca6b0f541ee79ac33221a11873d0c4660981ae
KaziMdMehediHasan <=>
2022-01-04 19:07:36 +0600

    redux-toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -3,6 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { cryptoApi } from '../services/cryptoApi';
 import { cryptoNewsApi } from '../services/cryptoNewsApi';
 
+// redux toolkit store
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
